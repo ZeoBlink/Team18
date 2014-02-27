d1bdf0dd4322227205bfc8faeda4a73d42f5ae51
sammileungg <sammileungg@gmail.com>
2014-02-27 12:38:06 -0800

    fixed navbar to align bottom.
    still have to fix space to the left of login

--- WorkFolder/JS/LittleHouseOfEvilPets.js
+++ WorkFolder/JS/LittleHouseOfEvilPets.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
 
+	//top header to align to bottom of head div (so that it aligns with logo)
+	$('#topheader').css({
+    	"position":'relative',
+    	"top": (($('#head').height()) - (($('#topheader').height() * 1.4)))
+	});
+
+	$(window).resize(function(){
+		var windowWidth = $(window).width();
+		if (windowWidth>1100){
+			$('#topheader').css({
+    			"position":'relative',
+    			"top": (($('#head').height()) - (($('#topheader').height() * 1.4)))
+			});
+		}
+		else if (1100>windowWidth && windowWidth>900){
+			$('#topheader').css({
+    			"position":'relative',
+    			"top": (($('#head').height()) - (($('#topheader').height() * 1.2)))
+			});
+		}
+		else if (900>windowWidth && windowWidth>700){
+			$('#topheader').css({
+    			"position":'relative',
+    			"top": (($('#head').height()) - (($('#topheader').height())))
+			});
+		}
+		else{
+			$('#topheader').css({
+    			"position":'relative'
+    		});
+		}
+	});
+
+	/* old navbar js
 	//hides subheader in nav bar at load of page
 	$(".sub-header").hide();
 
@@ -16,4 +50,5 @@ $(document).ready(function(){
 	$("#cart").click(function(){
 		$("#cart-sub").toggle();
 	});
+	*/
 });
